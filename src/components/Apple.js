c1ebc2b04ba469e327c6d40aa557efc785cdbdd9
B-Manish <batchumanish@gmail.com>
2024-08-11 23:21:25 +0530

    added landing section

--- src/components/Apple.js
+++ src/components/Apple.js
@@ -4,15 +4,24 @@ import { useGSAP } from "@gsap/react";
 import { Box } from "@mui/material";
 import "../App.css";
 import { heroVideo, smallHeroVideo } from "../utils";
+// if screnwidth is less than 760 PieChartOutlineSharp, then dusplay smallherovideo
 
 const Apple = () => {
   useGSAP(() => {
-    gsap.to("#apple", { opacity: 1, delay: 1 });
-    gsap.to("#highlights", { opacity: 1, delay: 1.5, y: -200 });
-    gsap.to(".aa", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+    gsap.to("#iphone", { opacity: 1, delay: 1.5 });
+    gsap.to("#gg", { opacity: 1, y: -50, delay: 1.5 });
   }, []);
   return (
-    <Box sx={{ width: "100%", background: "black", border: "1px solid red" }}>
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        height: "100vh",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Box
         sx={{
           opacity: "0",
@@ -20,40 +29,52 @@ const Apple = () => {
           fontWeight: "600",
           color: "#d7d5d1",
         }}
-        id="apple"
+        id="iphone"
+        className="roboto"
       >
         iPhone 15 Pro
       </Box>
-      {/* <Box sx={{ border: "1px solid red", width: "400px", height: "800px" }}>
+      <Box sx={{ width: "80%", maxWidth: "1200px" }}>
         <video
           autoPlay
           muted
           playsInline={true}
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
         >
-          <source src={smallHeroVideo} type="video/mp4" />
+          <source src={heroVideo} type="video/mp4" />
         </video>
       </Box>
       <Box
-        sx={{ opacity: "0", fontSize: "100px" }}
-        className="roboto"
-        id="highlights"
+        sx={{
+          opacity: "0",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: "300px",
+        }}
+        id="gg"
       >
-        Highlights
+        <Box
+          sx={{
+            background: "#005ab5",
+            padding: "11px 21px",
+            fontSize: "17px",
+            borderRadius: "980px",
+            cursor: "pointer",
+            width: "fit-content",
+            mb: "27px",
+          }}
+        >
+          Buy
+        </Box>
+        <Box
+          sx={{ fontSize: "21px", fontWeight: "600", color: "#d7d5d1" }}
+          className="roboto"
+        >
+          From ₹129800.00
+        </Box>
       </Box>
-      <Box sx={{ display: "flex" }}>
-        <Box sx={{ mr: "30px" }} className="aa">
-          Highlights
-        </Box>
-        <Box sx={{ display: "flex" }}>
-          <Box className="aa" sx={{ mr: "30px", opacity: "0" }}>
-            Watch the flim
-          </Box>
-          <Box className="aa" sx={{ mr: "30px", opacity: "0" }}>
-            Watch the flim
-          </Box>
-        </Box>
-      </Box> */}
     </Box>
   );
 };
