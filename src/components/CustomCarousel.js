f5b0c386ed0d7540c895067ac1891425f835ab9b
B-Manish <batchumanish@gmail.com>
2024-09-08 01:42:48 +0530

    Changes carousel

--- src/components/CustomCarousel.js
+++ src/components/CustomCarousel.js
@@ -15,10 +15,10 @@ const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const photoRefs = useRef([]);
 
-  // useEffect(() => {
-  //   // Fade in the first image on mount
-  //   gsap.from(photoRefs.current[currentIndex], { opacity: 0, duration: 1 });
-  // }, []);
+  useEffect(() => {
+    // Fade in the first image on mount
+    gsap.from(photoRefs.current[currentIndex], { opacity: 0, duration: 1 });
+  }, []);
 
   const goToNext = () => {
     const newIndex = currentIndex + 1;
@@ -43,7 +43,7 @@ const Carousel = () => {
       timeline.to(
         photo,
         {
-          x: offset * 600, // Each photo 300px apart
+          x: offset * 600, // Each photo 600px apart
           opacity: isCurrent ? 1 : 0.5,
           duration: 1,
           ease: "power2.inOut",
@@ -59,66 +59,67 @@ const Carousel = () => {
         height: "100vh",
         overflow: "hidden",
         backgroundColor: "black",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
-      <Box
-        sx={{
-          width: "600px",
-          height: "400px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          // overflow: "hidden",
-          border: "1px solid red",
-          marginTop: "calc(50% - 600px)",
-        }}
-      >
-        {photos.map((photo, index) => (
-          <img
-            key={index}
-            src={photo}
-            alt={`Slide ${index + 1}`}
-            ref={(el) => (photoRefs.current[index] = el)}
-            style={{
-              // transform: `translate(${(index - currentIndex) * 300}px, -50%)`,
-              width: "600px",
-              opacity: currentIndex === index ? 1 : 0.5,
-              transition: "opacity 0.5s ease",
-              border: "2px solid green",
-              margin: "0 20px",
-            }}
-          />
-        ))}
+      <Box>
+        <Box
+          sx={{
+            width: "600px",
+            height: "400px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            // overflow: "hidden",
+            marginTop: "calc(50% - 0px)",
+          }}
+        >
+          {photos.map((photo, index) => (
+            <img
+              key={index}
+              src={photo}
+              alt={`Slide ${index + 1}`}
+              ref={(el) => (photoRefs.current[index] = el)}
+              style={{
+                // transform: `translate(${(index - currentIndex) * 300}px, -50%)`,
+                width: "600px",
+                opacity: currentIndex === index ? 1 : 0.5,
+                transition: "opacity 0.5s ease",
+                margin: "0 20px",
+              }}
+            />
+          ))}
+        </Box>
+
+        <Button
+          disabled={currentIndex === 0 ? true : false}
+          onClick={goToPrev}
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+            },
+          }}
+        >
+          Previous
+        </Button>
+
+        <Button
+          disabled={currentIndex === 6 ? true : false}
+          onClick={goToNext}
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+            },
+          }}
+        >
+          Next
+        </Button>
       </Box>
-
-      <Button
-        disabled={currentIndex === 0 ? true : false}
-        onClick={goToPrev}
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          color: "black",
-          "&:hover": {
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-          },
-        }}
-      >
-        Previous
-      </Button>
-
-      <Button
-        disabled={currentIndex === 6 ? true : false}
-        onClick={goToNext}
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          color: "white",
-          "&:hover": {
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-          },
-        }}
-      >
-        Next
-      </Button>
-      {/* </Box> */}
     </Box>
   );
 };
